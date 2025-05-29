660800edcc0e9be495edbc5ada82a11387cd5878
Vanz-silva <162450963+mmlabb@users.noreply.github.com>
2025-05-29 11:59:28 -0300

    feat: componente pop up

---
+++ src/components/PopupBlog.js
@@ -0,0 +1,59 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
+
+const PopupBlog = () => {
+  const [isVisible, setIsVisible] = useState(true);
+
+  if (!isVisible) return null;
+  return (
+    <div className="popupContainer">
+      <div className="popupContent">
+        <button
+          onClick={() => setIsVisible(false)}
+          className="closeButton"
+          aria-label="Fechar pop-up"
+        >
+          ×
+        </button>
+
+        <img
+          src="/img/bookk.png"
+          alt="Img book"
+          width={78}
+          height={78}
+        />
+        <div className="popupTextGroup">
+          <h5 className="popupTitle">Acesse gratuitamente</h5>
+          <span className="popupText">Por tempo limitado!</span>
+        </div>
+        <Link
+          href="https://guiapratico.magmeta.com.br/"
+          className="popupButton"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <svg
+            width="16"
+            height="16"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M5 12H19M19 12L12 5M19 12L12 19"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default PopupBlog;
