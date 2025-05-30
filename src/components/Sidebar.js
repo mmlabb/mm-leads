57ded3cba04c9267f63021f6a14075110b165fc2
Vanz-silva <162450963+mmlabb@users.noreply.github.com>
2025-05-30 17:30:23 -0300

    feat: Add scrool side bar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,7 +9,7 @@ import {
 } from "@mui/material";
 import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
 import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
-
+import RocketLaunchIcon from '@mui/icons-material/RocketLaunch';
 import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import InstagramIcon from "@mui/icons-material/Instagram";
@@ -64,6 +64,8 @@ export default function CarouselForwardOnly() {
       direction="column"
       sx={{
         width: "492px",
+        height: "100%",
+        maxHeight: "100vh",
         alignItems: "center",
         justifyContent: "space-between",
         position: "relative",
@@ -93,122 +95,232 @@ export default function CarouselForwardOnly() {
         }}
       />
 
-      <Grid
-        className="carrousel"
-        container
-        direction="column"
+      <Box
         sx={{
-          p: "24px",
+          width: "100%",
+          height: "100%",
+          overflowY: "auto", // Habilita scroll vertical
+          padding: "24px",
           position: "relative",
           zIndex: 1,
-          width: "100%",
-          alignItems: "center",
-          justifyContent: "space-between",
-          gap: "24px",
-        }}
-      >
-        <Box
-          sx={{
-            width: 444,
-            height: 240,
-            overflow: "hidden",
-            borderRadius: 3,
-            position: "relative",
-          }}
-          onMouseEnter={() => setIsHovered(true)}
-          onMouseLeave={() => setIsHovered(false)}
-        >
-          <Box
-            sx={{
-              display: "flex",
-              width: `${maxSteps * 100}%`,
-              transform: animating
-                ? `translateX(-${(activeStep + 1) * (100 / maxSteps)}%)`
-                : `translateX(-${activeStep * (100 / maxSteps)}%)`,
-              transition: animating ? "transform 0.5s ease-in-out" : "none",
-            }}
-          >
-            {images.map((item, index) => (
-              <a
-                key={index}
-                href={item.url}
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{
-                  display: "block",
-                  width: `${100 / maxSteps}%`,
-                  height: "100%",
-                }}
-              >
-                <Box
-                  component="img"
-                  src={item.imgPath}
-                  alt={item.label}
-                  sx={{
-                    width: "100%",
-                    height: "100%",
-                    flexShrink: 0,
-                    objectFit: "contain",
-                    cursor: "pointer",
-                  }}
-                />
-              </a>
-            ))}
-          </Box>
-        </Box>
-      </Grid>
-
-      <Grid
-        container
-        justifyContent="center"
-        sx={{
-          mt: 1,
-          mb: 2,
-          position: "relative",
-          zIndex: 3,
-        }}
-      >
-        {images.map((_, index) => (
-          <Box
-            key={index}
-            onClick={() => setActiveStep(index)}
-            sx={{
-              width: 12,
-              height: 12,
-              borderRadius: "50%",
-              mx: 0.5,
-              backgroundColor:
-                index === activeStep ? "#fff" : "rgba(93, 46, 154, 0.3)",
-              transition: "background-color 0.3s",
-              cursor: "pointer",
-            }}
-          />
-        ))}
-      </Grid>
-
-      <Grid
-        sx={{
           display: "flex",
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "center",
-          pl: "24px",
-          pr: "24px",
-          mb: "24px",
-          gap: 3,
+          flexDirection: "column",
+          gap: "24px",
+          "&::-webkit-scrollbar": {
+            width: "6px",
+          },
+          "&::-webkit-scrollbar-track": {
+            background: "rgba(255, 255, 255, 0.1)",
+          },
+          "&::-webkit-scrollbar-thumb": {
+            backgroundColor: "rgba(98, 33, 162, 1)",
+            borderRadius: "3px",
+          },
         }}
       >
         <Grid
-          item
-          xs={7}
+          className="carrousel"
+          container
+          direction="column"
+          sx={{
+            position: "relative",
+            zIndex: 1,
+            width: "100%",
+            alignItems: "center",
+            justifyContent: "space-between",
+            gap: "24px",
+          }}
+        >
+          <Box
+            sx={{
+              width: 444,
+              height: 240,
+              overflow: "hidden",
+              borderRadius: 3,
+              position: "relative",
+            }}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                width: `${maxSteps * 100}%`,
+                transform: animating
+                  ? `translateX(-${(activeStep + 1) * (100 / maxSteps)}%)`
+                  : `translateX(-${activeStep * (100 / maxSteps)}%)`,
+                transition: animating ? "transform 0.5s ease-in-out" : "none",
+              }}
+            >
+              {images.map((item, index) => (
+                <a
+                  key={index}
+                  href={item.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  style={{
+                    display: "block",
+                    width: `${100 / maxSteps}%`,
+                    height: "100%",
+                  }}
+                >
+                  <Box
+                    component="img"
+                    src={item.imgPath}
+                    alt={item.label}
+                    sx={{
+                      width: "100%",
+                      height: "100%",
+                      flexShrink: 0,
+                      objectFit: "contain",
+                      cursor: "pointer",
+                    }}
+                  />
+                </a>
+              ))}
+            </Box>
+          </Box>
+        </Grid>
+
+        <Grid
+          container
+          justifyContent="center"
+          sx={{
+            mt: 1,
+            mb: 2,
+            position: "relative",
+            zIndex: 3,
+          }}
+        >
+          {images.map((_, index) => (
+            <Box
+              key={index}
+              onClick={() => setActiveStep(index)}
+              sx={{
+                width: 12,
+                height: 12,
+                borderRadius: "50%",
+                mx: 0.5,
+                backgroundColor:
+                  index === activeStep ? "#fff" : "rgba(93, 46, 154, 0.3)",
+                transition: "background-color 0.3s",
+                cursor: "pointer",
+              }}
+            />
+          ))}
+        </Grid>
+
+        <Grid
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+            gap: 3,
+          }}
+        >
+          <Grid
+            item
+            xs={7}
+            sx={{
+              display: { xs: "none", md: "flex" },
+              width: "210px",
+              p: "24px",
+              justifyContent: "center",
+              flexDirection: "column",
+              borderRadius: "18px",
+              color: "#fff",
+              backgroundColor: "rgba(255, 255, 255, 0.1)",
+              backdropFilter: "blur(147.9px)",
+              boxShadow: "0px 19px 30px rgba(0, 0, 0, 0.25)",
+            }}
+          >
+            <Typography
+              sx={{
+                fontFamily: "Inter, sans-serif",
+                fontWeight: "bold",
+                display: "flex",
+                alignItems: "center",
+              }}
+              variant="h5"
+            >
+              Soluções
+              <RocketLaunchIcon
+                sx={{
+                  margin: "auto",
+                  fontSize: "1.5rem", 
+                  color: "#fff", 
+                }}
+              />
+            </Typography>
+            <Typography>
+              Quer uma solução pronta para implementar na sua Empresa? Ela está
+              aqui!
+            </Typography>
+          </Grid>
+          <Grid
+            item
+            xs={5}
+            sx={{
+              display: { xs: "none", md: "flex" },
+              p: "24px",
+              justifyContent: "center",
+              flexDirection: "column",
+              borderRadius: "18px",
+              backgroundColor: "rgba(255, 255, 255, 0.1)",
+              backdropFilter: "blur(147.9px)",
+              boxShadow: "0px 19px 30px rgba(0, 0, 0, 0.25)",
+            }}
+          >
+            <Grid container sx={{ width: "100%", justifyContent: "center" }}>
+              {[
+                {
+                  icon: <FacebookOutlinedIcon sx={{ fontSize: 42 }} />,
+                  url: "https://www.facebook.com/profile.php?id=100089418330403",
+                },
+                {
+                  icon: <LinkedInIcon sx={{ fontSize: 42 }} />,
+                  url: "https://linkedin.com",
+                },
+                {
+                  icon: <InstagramIcon sx={{ fontSize: 42 }} />,
+                  url: "https://www.instagram.com/accounts/login/?next=https%3A%2F%2Fwww.instagram.com%2Fmagmetadigital%2F&is_from_rle",
+                },
+                {
+                  icon: <WhatsAppIcon sx={{ fontSize: 42 }} />,
+                  url: "https://api.whatsapp.com/send/?phone=5561981689203&text=Ol%C3%A1%2C%20equipe%20Magmeta%21%20Vim%20pelo%20Guia%20Pr%C3%A1tico%20e%20quero%20automatizar%20meu%20atendimento.",
+                },
+              ].map((item, idx) => (
+                <Grid
+                  key={idx}
+                  item
+                  xs={6}
+                  sx={{ display: "flex", justifyContent: "center" }}
+                >
+                  <IconButton
+                    component="a"
+                    href={item.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    sx={{ p: 1 }}
+                  >
+                    {item.icon}
+                  </IconButton>
+                </Grid>
+              ))}
+            </Grid>
+          </Grid>
+        </Grid>
+
+        <Grid
           sx={{
             display: { xs: "none", md: "flex" },
-            width: "210px",
+            width: "444px",
             p: "24px",
             justifyContent: "center",
             flexDirection: "column",
             borderRadius: "18px",
-            color: "#fff",
+            color: "#FFF",
             backgroundColor: "rgba(255, 255, 255, 0.1)",
             backdropFilter: "blur(147.9px)",
             boxShadow: "0px 19px 30px rgba(0, 0, 0, 0.25)",
@@ -222,126 +334,38 @@ export default function CarouselForwardOnly() {
               alignItems: "center",
             }}
             variant="h5"
+            component="h5"
           >
-            Soluções
-            <Box
-              component="img"
-              src="/img/icon-rocket.png"
-              alt="Ícone de foguete"
-              sx={{ marginLeft: "auto" }}
-            />
+            Seu Chat 100% automatizado!
           </Typography>
-          <Typography>
-            Quer uma solução pronta para implementar na sua Empresa? Ela está
-            aqui!
+
+          <Typography sx={{ maxWidth: "80%" }} variant="body1">
+            Você gostaria de ter em sua empresa um chat como este? que
+            automatiza seu atendimento e trás muito mais facilidade para o
+            fechamento da sua venda?
           </Typography>
-        </Grid>
-        <Grid
-          item
-          xs={5}
-          sx={{
-            display: { xs: "none", md: "flex" },
-            p: "24px",
-            justifyContent: "center",
-            flexDirection: "column",
-            borderRadius: "18px",
-            backgroundColor: "rgba(255, 255, 255, 0.1)",
-            backdropFilter: "blur(147.9px)",
-            boxShadow: "0px 19px 30px rgba(0, 0, 0, 0.25)",
-          }}
-        >
-          <Grid
-            container
-            spacing={4}
-            sx={{ width: "100%", justifyContent: "center" }}
+
+          <Button
+            component="a"
+            href="https://api.whatsapp.com/send/?phone=5561981689203&text=Ol%C3%A1%2C%20equipe%20Magmeta%21%20Vim%20pelo%20Guia%20Pr%C3%A1tico%20e%20quero%20automatizar%20meu%20atendimento."
+            target="_blank"
+            rel="noopener noreferrer"
+            fullWidth
+            sx={{
+              borderRadius: "24px",
+              background:
+                "linear-gradient(135deg, #db00ff, #6221a2) !important",
+              color: "#fff",
+              fontWeight: "bold",
+              textTransform: "none",
+              mt: 2,
+            }}
           >
-            {[
-              {
-                icon: <FacebookOutlinedIcon sx={{ fontSize: 42 }} />,
-                url: "https://facebook.com",
-              },
-              {
-                icon: <LinkedInIcon sx={{ fontSize: 42 }} />,
-                url: "https://linkedin.com",
-              },
-              {
-                icon: <InstagramIcon sx={{ fontSize: 42 }} />,
-                url: "https://instagram.com",
-              },
-              {
-                icon: <WhatsAppIcon sx={{ fontSize: 42 }} />,
-                url: "https://wa.me/",
-              },
-            ].map((item, idx) => (
-              <Grid
-                key={idx}
-                item
-                xs={6}
-                sx={{ display: "flex", justifyContent: "center" }}
-              >
-                <IconButton
-                  component="a"
-                  href={item.url}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  sx={{ p: 0 }}
-                >
-                  {item.icon}
-                </IconButton>
-              </Grid>
-            ))}
-          </Grid>
+            Criar meu Chat
+          </Button>
         </Grid>
-      </Grid>
-
-      <Grid
-        sx={{
-          display: { xs: "none", md: "flex" },
-          width: "444px",
-          p: "24px",
-          justifyContent: "center",
-          flexDirection: "column",
-          borderRadius: "18px",
-          color: "#FFF",
-          backgroundColor: "rgba(255, 255, 255, 0.1)",
-          backdropFilter: "blur(147.9px)",
-          boxShadow: "0px 19px 30px rgba(0, 0, 0, 0.25)",
-        }}
-      >
-        <Typography
-          sx={{
-            fontFamily: "Inter, sans-serif",
-            fontWeight: "bold",
-            display: "flex",
-            alignItems: "center",
-          }}
-          variant="h5"
-          component="h5"
-        >
-          Seu 100% Chat automatizado!
-        </Typography>
-
-        <Typography sx={{ maxWidth: "80%" }} variant="body1">
-          Você gostaria de ter em sua empresa um chat como este? que automatiza
-          seu atendimento e trás muito mais facilidade para o fechamento da sua
-          venda?
-        </Typography>
-
-        <Button
-          fullWidth
-          sx={{
-            borderRadius: "24px",
-            background: "linear-gradient(135deg, #db00ff, #6221a2) !important",
-            color: "#fff",
-            fontWeight: "bold",
-            textTransform: "none",
-            mt: 2,
-          }}
-        >
-          Criar meu Chat
-        </Button>
-      </Grid>
-      <PopupBlog />
+        <PopupBlog />
+      </Box>
     </Grid>
   );
 }
